5d8279b8cba96b6b7c8622fc76d0b33d7b2a9845
unknown <smuaq@yahoo.com>
2015-09-03 02:15:08 +0500

    initial commit

---
+++ js/agency.js
@@ -0,0 +1,45 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+
+    var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg'];
+    $('header').css({'background-image': 'url(./img/' + images[Math.floor(Math.random() *      images.length)] + ')'});
+
+    $(".owl-carousel").owlCarousel({
+        autoPlay: 2000,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Closes the Responsive Menu on Click outside Menu
+$('body > *').not('nav').click(function() {
+    if(!$('button.navbar-toggle').hasClass('collapsed')) {
+        $('.navbar-toggle:visible').click();
+    }
+});
